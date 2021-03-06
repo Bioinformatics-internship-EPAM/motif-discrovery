95f60def0eb8b5c2bbccf28d59184d5c7905341b
Maxim Solomonov <solomonov.ms@edu.spbstu.ru>
2021-03-06 11:10:31 +0300

    Added main class and main test class

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public String getGreeting() {
+        return "Hello, motif discovery";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
