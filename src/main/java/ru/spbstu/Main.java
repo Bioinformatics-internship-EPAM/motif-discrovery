2bf211e0960400f5a1bba6313171e23ee28546b8
AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
2021-03-21 22:23:55 +0300

    Add reading fasta file from cmd

--- src/main/java/ru/spbstu/Main.java
+++ src/main/java/ru/spbstu/Main.java
@@ -1,11 +1,29 @@
 package ru.spbstu;
 
-public class Main {
-    public String getGreeting() {
-        return "Hello, motif discovery";
-    }
+import org.apache.commons.cli.*;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        Options options = new Options();
+
+        Option input = new Option("p", "file-path", true, "Fasta file path");
+        input.setRequired(true);
+        options.addOption(input);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("motif-discovery", options);
+
+            System.exit(0);
+        }
+
+        String fastaFileName = cmd.getOptionValue("file-path");
+        System.out.println(fastaFileName);
     }
 }
