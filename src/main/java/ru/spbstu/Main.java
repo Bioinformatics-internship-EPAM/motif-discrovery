3b9c55a94450b7aa50398c967912f198a39d7b75
AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
2021-03-24 20:32:09 +0300

    Extract constants

--- src/main/java/ru/spbstu/Main.java
+++ src/main/java/ru/spbstu/Main.java
@@ -3,10 +3,15 @@ package ru.spbstu;
 import org.apache.commons.cli.*;
 
 public class Main {
+    private final static String OPTION = "file-path";
+    private final static String SHORT_OPTION = "p";
+    private final static String DESCRIPTION = "Fasta file path";
+    private final static String PROJECT = "motif-discovery";
+
     public static void main(String[] args) {
         Options options = new Options();
 
-        Option input = new Option("p", "file-path", true, "Fasta file path");
+        Option input = new Option(SHORT_OPTION, OPTION, true, DESCRIPTION);
         input.setRequired(true);
         options.addOption(input);
 
@@ -18,12 +23,12 @@ public class Main {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("motif-discovery", options);
+            formatter.printHelp(PROJECT, options);
 
             System.exit(0);
         }
 
-        String fastaFileName = cmd.getOptionValue("file-path");
+        String fastaFileName = cmd.getOptionValue(OPTION);
         System.out.println(fastaFileName);
     }
 }
